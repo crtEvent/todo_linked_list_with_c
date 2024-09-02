11574dd1cf43677aeb1c431e756eecece04ce465
Ju, Seonghwan <crtevn@gmail.com>
2024-09-02 16:11:42 +0900

    노드 추가하기 함수 추가

--- main.c
+++ main.c
@@ -10,7 +10,24 @@ typedef struct TODODATA {
 
 TODODATA *g_pHeadNode = NULL;
 
+void AddNewNode(const char *pszTask, const char *pszDue) {
+    TODODATA *pNewNode = (TODODATA *)malloc(sizeof(TODODATA));
+    strcpy(pNewNode->task, pszTask);
+    strcpy(pNewNode->due, pszDue);
+    pNewNode->pNext = NULL;
+
+    if (g_pHeadNode == NULL) {
+        g_pHeadNode = pNewNode;
+    } else {
+        pNewNode->pNext = g_pHeadNode;
+        g_pHeadNode = pNewNode;
+    }
+}
+
 int main() {
+    AddNewNode("Task 1", "2024-09-01");
+    AddNewNode("Task 2", "2024-09-02");
+
     TODODATA *pTmp = g_pHeadNode;
     while (pTmp != NULL) {
         printf("[%p] %s, %s [%p]\n", 
